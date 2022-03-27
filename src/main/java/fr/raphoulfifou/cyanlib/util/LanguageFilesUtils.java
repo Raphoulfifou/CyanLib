b373999ead299e94a73b40c364f1d7e211854a45
Raphoulfifou <raphael.roger.92@gmail.com>
2022-03-27 11:35:28 +0200

    new file system for languages

---
+++ src/main/java/fr/raphoulfifou/cyanlib/util/LanguageFilesUtils.java
@@ -0,0 +1,80 @@
+package fr.raphoulfifou.cyanlib.util;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.TranslatableText;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static fr.raphoulfifou.cyanlib.util.ChatUtil.sendPlayerMessage;
+
+public class LanguageFilesUtils
+{
+
+    public static void setupLangFile(ServerPlayerEntity player, String folderName, String langName, boolean useOneLanguage)
+    {
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER)
+        {
+            if (! Files.exists(FabricLoader.getInstance().getConfigDir().resolve(folderName)))
+            {
+                try
+                {
+                    Files.createDirectory(Paths.get(String.valueOf(FabricLoader.getInstance().getConfigDir().resolve(folderName))));
+                    sendPlayerMessage(player,
+                            "Folder created : config/" + folderName + "/",
+                            folderName,
+                            "cyan.message.folderCreated",
+                            false,
+                            useOneLanguage);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            try
+            {
+                File langFile = new File(String.valueOf(FabricLoader.getInstance().getConfigDir().resolve(folderName + "\\" + langName + ".txt")));
+                if (langFile.createNewFile())
+                {
+                    sendPlayerMessage(player,
+                            "File created: " + langFile.getName(),
+                            langFile.getName(),
+                            "cyan.message.fileCreated",
+                            false,
+                            useOneLanguage);
+                } else
+                {
+                    player.sendMessage(new TranslatableText("File already exists."), false);
+                    sendPlayerMessage(player,
+                            "File already exists.",
+                            null,
+                            "cyan.message.fileAlreadyExists",
+                            false,
+                            useOneLanguage);
+                }
+            } catch (IOException e)
+            {
+                sendPlayerMessage(player,
+                        "Error",
+                        null,
+                        "cyan.message.error",
+                        false,
+                        useOneLanguage);
+                e.printStackTrace();
+            }
+        } else
+        {
+            sendPlayerMessage(player,
+                    "This functionnality only works if you are on a server",
+                    null,
+                    "cyan.message.notOnServer",
+                    false,
+                    useOneLanguage);
+        }
+    }
+
+}
